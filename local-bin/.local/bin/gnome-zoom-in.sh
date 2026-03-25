83d58ddd3d6990710e58ada4672b79ba7ab276f0
WillScarlettOhara <39462014+WillScarlettOhara@users.noreply.github.com>
2026-03-25 02:51:36 +0100

    Auto Backup: 2026-03-25 02:51

--- local-bin/.local/bin/gnome-zoom-in.sh
+++ local-bin/.local/bin/gnome-zoom-in.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CURRENT=$(gsettings get org.gnome.desktop.a11y.magnifier mag-factor)
-gsettings set org.gnome.desktop.a11y.magnifier mag-factor $(echo "$CURRENT + 0.5" | bc)
+gsettings set org.gnome.desktop.a11y.magnifier mag-factor "$(echo "$CURRENT + 0.5" | bc)"
