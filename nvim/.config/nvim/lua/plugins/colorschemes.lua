e99a417b27eb539f236e88103ebc63923230761a
WillScarlettOhara <39462014+WillScarlettOhara@users.noreply.github.com>
2026-03-26 20:42:33 +0100

    transparence de la cursorline enfin trouvée dans ghostty

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -64,13 +64,16 @@ return {
     "sainnhe/gruvbox-material",
     priority = 1000,
     config = function()
-      -- vim.g.gruvbox_material_transparent_background = 1
+      vim.g.gruvbox_material_transparent_background = 2
       vim.g.gruvbox_material_foreground = "original"
       vim.g.gruvbox_material_background = "hard"
       vim.g.gruvbox_material_ui_contrast = "high"
       vim.g.gruvbox_material_float_style = "blend"
       vim.g.gruvbox_material_statusline_style = "default"
       vim.g.gruvbox_material_cursor = "auto"
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
     end,
   },
 
