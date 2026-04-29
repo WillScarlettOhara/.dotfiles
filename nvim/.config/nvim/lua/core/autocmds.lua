8549c52eff8f7a6bab8fa8cbc9a7135e7a029308
WillScarlettOhara <39462014+WillScarlettOhara@users.noreply.github.com>
2026-04-29 18:17:27 +0200

    disable spelling on txt/md/tex by default

--- nvim/.config/nvim/lua/core/autocmds.lua
+++ nvim/.config/nvim/lua/core/autocmds.lua
@@ -48,13 +48,13 @@ api.nvim_create_autocmd(
 )
 
 -- Enable spell checking for certain file types
-api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  pattern = { "*.txt", "*.md", "*.tex" },
-  callback = function()
-    vim.opt.spell = true
-    vim.opt.spelllang = "en"
-  end,
-})
+-- api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+--   pattern = { "*.txt", "*.md", "*.tex" },
+--   callback = function()
+--     vim.opt.spell = true
+--     vim.opt.spelllang = "en"
+--   end,
+-- })
 
 -- close some filetypes with <q>
 api.nvim_create_autocmd("FileType", {
