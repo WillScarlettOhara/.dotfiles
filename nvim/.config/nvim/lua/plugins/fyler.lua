cf228519af461f55ad594000011fd7fbc05a3d22
WillScarlettOhara <39462014+WillScarlettOhara@users.noreply.github.com>
2026-05-05 03:34:52 +0200

    flyer helper

--- nvim/.config/nvim/lua/plugins/fyler.lua
+++ nvim/.config/nvim/lua/plugins/fyler.lua
@@ -22,6 +22,39 @@ return {
         win = {
           kind = "float",
         },
+        mappings = {
+          ["<BS>"] = "GotoParent",
+          ["?"] = function()
+            local lines = {
+              "  q          Close Fyler",
+              "  <CR>       Open file",
+              "  <C-t>      Open in new tab",
+              "  |          Open in vsplit",
+              "  -          Open in split",
+              "  <BS>       Go to parent dir",
+              "  ^          Go to cwd",
+              "  .          Go to node",
+              "  #          Collapse all",
+              "  a          Add file/dir",
+              "  d          Delete",
+              "  r          Rename",
+              "  c          Copy",
+              "  m          Move",
+              "  o          Open with system app",
+              "  y          Yank",
+              "  p          Paste",
+              "  u          Update",
+              "  I          Toggle ignored",
+              "  H          Toggle hidden",
+              "  Z          Close all dirs",
+            }
+            Snacks.notify(table.concat(lines, "\n"), {
+              title = "Fyler Keymaps",
+              timeout = 15000,
+              icon = "󰌌",
+            })
+          end,
+        },
       },
     },
   },
